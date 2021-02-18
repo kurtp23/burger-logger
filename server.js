1af0a73d458356efe348fc7424fdd7aa0ee88381
kurtp23 <kurt_phillips@live.com>
2021-02-18 12:56:43 -0800

    heoku stuff

--- server.js
+++ server.js
@@ -12,13 +12,17 @@ app.use(express.static("pub"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Password",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Password",
+    database: "burgers_db",
+  });
+}
 connection.connect(function (err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
